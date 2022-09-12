0f365774006b06328065c7d02a17d35302e552a6
dabhade904 <dabhade904@gmail.com>
2022-09-12 19:07:14 +0530

    Added : Address Book Attributes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AddressBookLinq
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace AddressBookLinq
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome");
+        {           
+            while (true)
+            {
+               
+            }
         }
     }
 }
